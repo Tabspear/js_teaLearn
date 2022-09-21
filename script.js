2ce58919b0c80c4cac54b86adf4ac44eb92ac6a9
tabspear <tabspear@github.com>
2022-09-20 21:17:00 -0700

    Day One Take Home

---
+++ script.js
@@ -0,0 +1,15 @@
+/**
+ * Single line comment
+Multi-line comment
+Log your name to the console
+Log your favorite food to the console
+ */
+
+//Hello There
+/*
+Whenever you are there
+you make me feel happy
+*/
+
+console.log("Taofeek Badmus")
+console.log("Rice")

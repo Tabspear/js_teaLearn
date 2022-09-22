20d435a308fa43aa401a8257da1a5732b260fbdb
tabspear <tabspear@github.com>
2022-09-22 13:55:27 -0700

    Feat(client): Daily Challenge Day 2

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-/**
- * Single line comment
-Multi-line comment
-Log your name to the console
-Log your favorite food to the console
- */
+//DAY ONE 
 
 //Hello There
 /*
@@ -13,3 +8,11 @@ you make me feel happy
 
 console.log("Taofeek Badmus")
 console.log("Rice")
+
+//DAY TWO
+let letters = "Supercalifragilisticexpialidocious"
+console.log(letters.length)
+console.log(50)
+let BigNumber = 33333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333n
+console.log(typeof(BigNumber))
+console.log(BigNumber)
